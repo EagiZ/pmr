b5239780995702472e90065ca2807e751aa11000
Mikael Östlund <eagiz@live.se>
2014-06-18 23:40:00 +0200

    Added basic structure for Player model

---
+++ frontend/src/models/Player.java
@@ -0,0 +1,34 @@
+package models;
+
+/**
+ * The player class. Represents a player and contains information
+ * about its position.
+ */
+public class Player {
+	private float x;
+	private float y;
+	private float accel;
+	
+	private String name;
+	
+	/**
+	 * @param x				The player's position on the x-axis
+	 * @param y				The player's position on the y-axis
+	 * @param playerName	The player name
+	 */
+	public Player(int x, int y, String playerName) {
+		this.x = x;
+		this.y = y;
+		this.name = playerName;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	
+}
