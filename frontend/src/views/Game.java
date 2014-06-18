dc4e1cef4be03cc514fbba684809fe0b71b4e23b
Mikael Östlund <eagiz@live.se>
2014-06-19 01:08:26 +0200

    Added some connection test to Game

--- frontend/src/views/Game.java
+++ frontend/src/views/Game.java
@@ -1,9 +1,14 @@
 package views;
 
+import controllers.*;
+
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
 
 public class Game extends BasicGame {
 	Input input;
+	Circle circle;
+	Connection testCon = new Connection("127.0.0.1");
 	
 	public Game() {
         super("Putt my redneck");
@@ -11,18 +16,27 @@ public class Game extends BasicGame {
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		
+		circle = new Circle(400.0f, 400.0f, 20.0f);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-
+		input = container.getInput();
+		
+		float newX = input.getMouseX();
+		float newY = input.getMouseY();
+				
+		circle.setCenterX(newX);
+		circle.setCenterY(newY);
+		
+		testCon.send("move " + "newX: " + newX + " newY: " + newY + "\n");
+		System.out.println(testCon.receive());
 	}
 
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-
+		g.draw(circle);
 	}
 	
 	public static void main(String[] arguments) {
