2439c35388ef5dfcba88b203b83842a38f15004e
Gojs <djalekzse@gmail.com>
2014-06-20 22:47:58 +0200

    Server.hs is now working with JSON

--- backend/Server.hs
+++ backend/Server.hs
@@ -2,6 +2,9 @@ import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
 import System.Environment (getArgs)
 import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
 import Control.Concurrent (forkIO)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.ByteString.Char8 as C
+import User
 
 -- Notes:
 -- hPutStrLn is used to send data back to the client
@@ -19,19 +22,23 @@ sockHandler :: Socket -> IO ()
 sockHandler sock = do
   (clientHandle, _HostName, _PortNumber) <- accept sock
   hSetBuffering clientHandle NoBuffering
+  putStrLn "User connected."
   forkIO $ commandProcessor clientHandle -- start commandProcessor on new thread
   sockHandler sock
 
 commandProcessor :: Handle -> IO ()
 commandProcessor clientHandle = do
-  line <- hGetLine clientHandle
-  let cmd = words line
-  case (head cmd) of
-    ("move") -> do
-      putStrLn "recieved move command"
-      moveCommand clientHandle cmd
-    _ -> do
-      hPutStrLn clientHandle "Unknown command"
+  fromClient <- hGetLine clientHandle
+  let bsFromClient = BS.fromStrict $ C.pack fromClient
+  let decoded = decodeJSON bsFromClient :: Either String User
+  case decoded of
+    Left err -> do
+      putStrLn err
+      hPutStrLn clientHandle err
+    Right user -> do
+      testPrint user
+      let newUser' = C.unpack $ BS.toStrict $ encodeJSON user
+      hPutStrLn clientHandle newUser'
   commandProcessor clientHandle
 
 moveCommand :: Handle -> [String] -> IO ()
@@ -41,4 +48,7 @@ moveCommand clientHandle cmd = do
 connectCommand :: Handle -> [String] -> IO ()
 connectCommand clientHandle cmd = do
   hPutStrLn clientHandle (unwords $ tail cmd) -- TODO: do something valuable
-  
+
+testPrint :: User -> IO ()
+testPrint (User userID userName score xPos yPos xVel yVel radius) =
+  putStrLn ("xPos: " ++ show xPos ++ " - yPos: " ++ show yPos)
