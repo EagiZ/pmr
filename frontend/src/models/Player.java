f3e21a184ea7abe1f55b4a748f4d70eecc93816a
Mikael Östlund <eagiz@live.se>
2014-06-21 00:32:37 +0200

    Added json-handler to Player, methods toJSON and fromJSON added

--- frontend/src/models/Player.java
+++ frontend/src/models/Player.java
@@ -3,6 +3,9 @@ package models;
 import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Vector2f;
 
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
 /**
  * The player class. Represents a player and contains information
  * about its position, hitbox, velocity et.c.
@@ -11,10 +14,12 @@ public class Player {
 	
 	/* === Fields === */
 
+	private int userID = -1;
+	private String playerName = "undefined";
 	private Circle hitbox;
 	private Vector2f velocity;
 	private float acceleration;
-	private String playerName;
+	private int score = 0;
 	
 	/* === Constructors === */
 	
@@ -28,6 +33,61 @@ public class Player {
 		velocity = new Vector2f(0, 0);
 		acceleration = 0.9973f;
 		this.setPlayerName(playerName);
+	}
+	
+	/**
+	 * Converts a JsonObject to a Player object.
+	 * 
+	 * @param object	JsonObject representing a Player.
+	 * @return a Player instance corresponding to the JsonValues in object. 
+	 */
+	public static Player fromJSON(JsonObject object) {
+		int userID = object.get("userID").asInt();
+		String userName = object.get("userName").toString();
+		int score = object.get("score").asInt();
+		float xPos = object.get("xPos").asFloat();
+		float yPos = object.get("yPos").asFloat();
+		float xVel = object.get("xVel").asFloat();
+		float yVel = object.get("yVel").asFloat();
+		float radius = object.get("radius").asFloat();
+		
+		Player player = new Player((int) xPos, (int) yPos, (int) radius, userName);
+		Vector2f velocityVector = new Vector2f(xVel, yVel);
+		
+		player.setVelocity(velocityVector);
+		player.setScore(score);
+		player.setUserID(userID);
+		
+		return player;
+	}
+	
+	/**
+	 * Converts a Player object to JsonObject.
+	 * 
+	 * @param player Player object representing a Player.
+	 * @return a JsonObject representing player. 
+	 */
+	public static JsonObject toJSON(Player player) {
+		int userID = player.getUserID();
+		String userName = player.getPlayerName();
+		int score = player.getScore();
+		float xPos = player.getPosition().getX();
+		float yPos = player.getPosition().getY();
+		float xVel = player.getVelocity().getX();
+		float yVel = player.getVelocity().getY();
+		float radius = player.getHitbox().getRadius();
+		
+		JsonObject jsonObject = new JsonObject()
+		.add("userID", userID)
+		.add("userName", userName)
+		.add("score", score)
+		.add("xPos", xPos)
+		.add("yPos", yPos)
+		.add("xVel", xVel)
+		.add("yVel", yVel)
+		.add("radius", radius);
+		
+		return jsonObject;
 	}
 	
 	/* === Accessors === */
@@ -63,5 +123,21 @@ public class Player {
 	public void update() {
 		hitbox.setLocation(hitbox.getLocation().add(velocity));
 		velocity.scale(acceleration);
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
 	}
 }
