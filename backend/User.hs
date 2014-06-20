149c1d1dd8829d5a05c888dcfbf48eacf373a21e
Mikael Östlund <eagiz@live.se>
2014-06-20 03:24:12 +0200

    Started on User module, still a lot to do

---
+++ backend/User.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson
+import Data.Text
+
+import Control.Applicative ((<$>), (<*>), empty)
+import Control.Monad
+import qualified Data.ByteString.Lazy as BS
+import GHC.Generics
+
+
+data User = User { userID :: Int,
+                   userName :: String,
+                   score :: Int,
+                   xPos :: Float,
+                   yPos :: Float,
+                   xVel :: Float,
+                   yVel :: Float,
+                   radius :: Float
+                 } deriving (Show)
+
+-- Instances to convert our type to/from JSON.
+instance FromJSON User where
+  parseJSON (Object v) =
+    User <$> v .: "userID"
+    <*> v .: "userName"
+    <*> v .: "score"
+    <*> v .: "xPos"
+    <*> v .: "yPos"
+    <*> v .: "xVel"
+    <*> v .: "yVel"
+    <*> v .: "radius"
+  parseJSON _ = Control.Applicative.empty
+
+instance ToJSON User where
+  toJSON (User userID userName score xPos yPos xVel yVel radius) =
+    object [ "userID"   .= userID
+           , "userName" .= userName
+           , "score"    .= score
+           , "xPos"     .= xPos
+           , "yPos"     .= yPos
+           , "xVel"     .= xVel
+           , "yVel"     .= yVel
+           , "radius"   .= radius
+           ]
--- backend/server.hs
+++ backend/server.hs
@@ -3,16 +3,6 @@ import System.Environment (getArgs)
 import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
 import Control.Concurrent (forkIO)
 
--- User contains of UserID (Int), UserName (String), x-axis position (Float), y-axis position (Float)
-data User = User { userID :: Int,
-                   userName :: String,
-                   score :: Int,
-                   xPos :: Float,
-                   yPos :: Float,
-                   xVel :: Float,
-                   yVel :: Float
-                 } deriving (Show)
-
 -- Notes:
 -- hPutStrLn is used to send data back to the client
 -- hGetLine reads data from client
