fc57f7eefbf2e3515ba8fd63bef7733e2de9d110
Mikael Östlund <eagiz@live.se>
2014-07-04 01:34:15 +0200

    Connect-message implementation started

--- backend/Server.hs
+++ backend/Server.hs
@@ -8,7 +8,7 @@ import qualified Data.ByteString.Char8 as C
 import User
 
 
-type Msg = String
+type Msg = (String, Chan String) -- TODO: Should probably be 'Either (Chan String) Bool'
 
 -- Notes:
 -- hPutStrLn is used to send data back to the client
@@ -23,7 +23,7 @@ main = withSocketsDo $ do -- withSocketsDo required for Windows usage
   putStrLn $ "Spawning supervisor"
   broadcastChan <- newChan
   messageBox <- newChan
-  _ <- forkIO $ supervisor broadcastChan messageBox
+  _ <- forkIO $ supervisor broadcastChan messageBox []
   sockHandler socket broadcastChan messageBox
 
 sockHandler :: Socket -> Chan Msg -> Chan Msg -> IO ()
@@ -31,49 +31,76 @@ sockHandler sock broadcastChan messageBox = do
   (clientHandle, _HostName, _PortNumber) <- accept sock
   hSetBuffering clientHandle NoBuffering
   putStrLn "User connected."
-  _ <- forkIO $ commandProcessor clientHandle -- Start commandProcessor in new thread
+  _ <- forkIO $ commandProcessor clientHandle messageBox -- Start commandProcessor in new thread
   broadcastChan' <- dupChan broadcastChan
   _ <- forkIO $ recieveLoop clientHandle broadcastChan' messageBox -- Start recieveLoop in new thread
-  let broadcast msg = writeChan broadcastChan' msg
-  broadcast "Supervisor testing channel"
   sockHandler sock broadcastChan messageBox
 
-supervisor :: Chan Msg -> Chan Msg -> IO()
-supervisor broadcastChan messageBox = do
-  line <- readChan messageBox
+createUniqueID :: [User] -> [Int] -> Int
+createUniqueID [] candidates =
+  head candidates
+createUniqueID (user@(User userID username score xPos yPos xVel yVel acc radius isAlive):tl) candidates =
+  createUniqueID tl $ filter (/=userID) candidates 
+
+supervisor :: Chan Msg -> Chan Msg -> [User] -> IO()
+supervisor broadcastChan messageBox userL = do
+  (line, chan) <- readChan messageBox
   let cmd = words line
   case (head cmd) of
     ("move") ->
       putStrLn "supervisor: move tbi"
-    ("connect") ->
-      putStrLn "supervisor: connect tbi"
+    ("connect") -> do
+      let playerAsJSONStr = unwords $ tail cmd
+      let bsFromClient = BS.fromStrict $ C.pack playerAsJSONStr
+      let decoded = decodeJSON bsFromClient :: Either String User
+      case decoded of
+        Left err -> do
+          putStrLn err
+        Right user@(User userID username score xPos yPos xVel yVel acc radius isAlive) -> do
+          let newUserID = createUniqueID userL [1..(length userL + 2)]
+          let (tempX, tempY) = (80.0 * fromIntegral(newUserID), 400.0) -- TODO: temp solution used for dev purposes
+          let tempRadius = 20.0
+          let createdUser = User newUserID username 0 tempX tempY 0.0 0.0 acc tempRadius False
+          testPrint user
+          testPrint createdUser
+          let userAsJSON = C.unpack $ BS.toStrict $ encodeJSON createdUser
+          writeChan chan userAsJSON
     ("disconnect") ->
       putStrLn "supervisor: disconnect tbi"
     _ -> 
       putStrLn "recieved message in messageBox"
-  let broadcast msg = writeChan broadcastChan msg
-  broadcast "Supervisor testing channel"
-  supervisor broadcastChan messageBox
+  supervisor broadcastChan messageBox userL
 
-commandProcessor :: Handle  -> IO ()
-commandProcessor clientHandle  = do
+commandProcessor :: Handle -> Chan Msg  -> IO ()
+commandProcessor clientHandle messageBox  = do
   fromClient <- hGetLine clientHandle
-  let bsFromClient = BS.fromStrict $ C.pack fromClient
-  let decoded = decodeJSON bsFromClient :: Either String User
-  case decoded of
-    Left err -> do
-      putStrLn err
-      hPutStrLn clientHandle err
-    Right user -> do
-      testPrint user
-      let newUser' = C.unpack $ BS.toStrict $ encodeJSON user
-      hPutStrLn clientHandle newUser'
-  commandProcessor clientHandle
+  let cmd = words fromClient
+  case (head cmd) of
+    ("connect") -> do
+      recieveChannel <- newChan
+      writeChan messageBox (fromClient, recieveChannel)
+      line <- readChan recieveChannel
+      hPutStrLn clientHandle line
+    ("refresh") -> do
+      -- TODO: parse tail cmd (which should be JSON-array containing User objects) etc.
+      putStrLn "recieveLoop recieved refresh-message"
+    _ -> do -- TODO
+      let bsFromClient = BS.fromStrict $ C.pack fromClient
+      let decoded = decodeJSON bsFromClient :: Either String User
+      case decoded of
+        Left err -> do
+          putStrLn err
+          hPutStrLn clientHandle err
+        Right user -> do
+          testPrint user
+          let newUser' = C.unpack $ BS.toStrict $ encodeJSON user
+          hPutStrLn clientHandle newUser'
+  commandProcessor clientHandle messageBox
 
 -- a recieve loop that should be created along with a commandProcessor with the same handle
 recieveLoop :: Handle -> Chan Msg -> Chan Msg -> IO ()
 recieveLoop clientHandle broadcastChan messageBox = do
-  line <- readChan broadcastChan
+  (line, chan) <- readChan broadcastChan
   let cmd = words line
   case (head cmd) of
     ("refresh") -> do
@@ -81,7 +108,7 @@ recieveLoop clientHandle broadcastChan messageBox = do
       putStrLn "recieveLoop recieved refresh-message"
     _ -> do
       putStrLn "recieveLoop recieved unkown command"
-      writeChan messageBox "hello supervisor"
+      --writeChan messageBox "hello supervisor"
   recieveLoop clientHandle broadcastChan messageBox
 
 moveCommand :: Handle -> [String] -> IO ()
@@ -93,5 +120,5 @@ connectCommand clientHandle cmd = do
   hPutStrLn clientHandle (unwords $ tail cmd) -- TODO: do something valuable
 
 testPrint :: User -> IO ()
-testPrint (User userID username score xPos yPos xVel yVel radius isAlive) =
+testPrint (User userID username score xPos yPos xVel yVel acc radius isAlive) =
   putStrLn ("xPos: " ++ show xPos ++ " - yPos: " ++ show yPos)
